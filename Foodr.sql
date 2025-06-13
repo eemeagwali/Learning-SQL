9cd7508254b904e66a0bc35529360149264a8be9
eemeagwali <ifeonline2001@yahoo.com>
2025-06-12 22:45:02 -0700

    Query to calculate revenue

--- Foodr.sql
+++ Foodr.sql
@@ -1,1 +1,9 @@
 SELECT * FROM meals
+
+--1. Calculating Revenue 
+SELECT  
+  order_id,  
+  SUM(meal_price * order_quantity) AS revenue  
+FROM meals  
+JOIN orders ON meals.meal_id = orders.meal_id  
+GROUP BY order_id;
